026d0336a51edf08e909873678d4bda9fd6c22a4
Dominus20 <102250448+Dominus20@users.noreply.github.com>
2022-07-05 20:50:01 +0300

    05.07.2022

---
+++ src/main/java/dto/ErrorDto.java
@@ -0,0 +1,17 @@
+package dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+
+public class ErrorDto {
+    int code;
+    String details;
+    String message;
+}
